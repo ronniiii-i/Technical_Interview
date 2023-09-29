8eb4304d0914d648c00f20bdad946af61f36fcbc
Roni <ronniiii@yahoo.com>
2023-09-29 13:52:47 +0100

    [UPDATE]create search component and implement functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import "./App.scss";
 function App() {
   const [users, setUsers] = useState([]);
   const [editingUser, setEditingUser] = useState(null);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searched, setSearched] = useState(false);
 
   useEffect(() => {
     const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
@@ -24,7 +26,6 @@ function App() {
     setUsers(users);
     setEditingUser(null);
   };
-
   const editUser = (id) => {
     const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
     const userToEdit = storedUsers.find((user) => user.id === id);
@@ -32,18 +33,46 @@ function App() {
       setEditingUser(userToEdit);
     }
   };
-
   const deleteUser = (id) => {
     const updatedUsers = users.filter((user) => user.id !== id);
     setUsers(updatedUsers);
     localStorage.setItem("users", JSON.stringify(updatedUsers));
   };
 
+  const handleSearch = (searchTerm) => {
+    const filtered = users.filter(
+      (user) =>
+        user.fname.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.lname.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+    console.log(filtered);
+    console.log(filteredUsers);
+    if (searchTerm != "") {
+      setSearched(true)
+    } else (
+      setSearched(false)
+    )
+  };
+
   return (
     <main>
-      <Form users={users} addUser={addUser} updateUsers={updateUsers} editingUser={editingUser} />
-      <Search />
-      <Table users={users} deleteUser={deleteUser} editUser={editUser} />
+      <Form
+        users={users}
+        addUser={addUser}
+        updateUsers={updateUsers}
+        editingUser={editingUser}
+      />
+      <Search handleSearch={handleSearch} />
+      <Table
+        users={users}
+        det={filteredUsers.length > 0 ? "filtered" : "not"}
+        filtered={filteredUsers}
+        searched={searched}
+        deleteUser={deleteUser}
+        editUser={editUser}
+      />
     </main>
   );
 }
